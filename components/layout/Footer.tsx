6efb68006553684df38a69b1d1e46dcbd4d46118
hieuvovan <hieuvo0904@gmail.com>
2022-11-26 15:10:29 +0700

    implement footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -4,7 +4,21 @@ export interface IFooterProps {}
 
 // eslint-disable-next-line react/display-name
 const Footer = React.forwardRef((props: IFooterProps, ref: any) => {
-  return <div ref={ref}>This is Footer</div>;
+  return (
+    <footer
+      className="bg-gray-100"
+      ref={ref}
+    >
+      <div className="container mx-auto">
+        <div className="footer-wrapper py-4">
+          <p className="text-center max-w-2xl mx-auto">
+            DEV Community 👩‍💻👨‍💻 — A constructive and inclusive social network for
+            software developers. With you every step of your journey.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 });
 
 export default Footer;
