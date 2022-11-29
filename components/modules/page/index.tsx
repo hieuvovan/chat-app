17763bfddccd7f0f518f1bd7654f3ba2481afef5
hieuvovan <hieuvo0904@gmail.com>
2022-11-29 16:25:52 +0700

    scroll to top

--- components/modules/page/index.tsx
+++ components/modules/page/index.tsx
@@ -7,5 +7,12 @@ export interface IPageProps {
 export const Page = (props: IPageProps) => {
   const { children } = props;
 
+  React.useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }, []);
+
   return <div className="pb-10 pt-24">{children}</div>;
 };
